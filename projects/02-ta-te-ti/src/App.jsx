a88764e290340b9361efdf8e0411749bc054e5d1
Sebastián Flores <sebflodev@gmail.com>
2023-09-19 22:24:58 -0300

    Commit inicial

---
+++ projects/02-ta-te-ti/src/App.jsx
@@ -0,0 +1,57 @@
+import { useState } from 'react'
+import './App.css'
+
+// Primero los turnos
+const TURNS = {
+  X: 'x',
+  O: 'o'
+}
+
+// Segundo el tablero
+// const board = Array(9).fill(null)
+
+// Los cuadrados
+const Square = ({ children, updateBoard, isSelected, index }) => {
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+  return (
+    <div className={className}>
+      {children}
+    </div>
+  )
+}
+
+function App() {
+  const [board, setBoard] = useState(Array(9).fill(null));
+  const [turn, setTurn] = useState(TURNS.X)
+
+  return (
+    <main className='board'>
+      <h1>Ta Te Ti</h1>
+      <section className='game'>
+        {
+          board.map((_, index) => {
+            return (
+              <Square 
+                key={index}
+                index={index}
+              >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+
+      <section className='turn'>
+        <Square isSelected={turn === TURNS.X}>
+          {TURNS.X}
+        </Square>
+        <Square isSelected={turn === TURNS.O}>
+          {TURNS.O}
+        </Square>
+      </section>
+    </main>
+  )
+}
+
+export default App
