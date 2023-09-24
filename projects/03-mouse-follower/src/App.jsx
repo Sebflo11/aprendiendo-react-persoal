3a7fa450b8f8bf86ac8671fb1a98d59bb51597dc
Sebastián Flores <sebflodev@gmail.com>
2023-09-24 20:19:42 -0300

    Update repo with third proyect

--- projects/03-mouse-follower/src/App.jsx
+++ projects/03-mouse-follower/src/App.jsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react"
 
-function App() {
+const FollowMouse = () => {
   const [enabled, setEnabled] = useState(false)
   const [position, setPosition] = useState({x: 0, y: 0})
 
@@ -14,18 +14,28 @@ function App() {
     if (enabled) {
       window.addEventListener('pointermove', handleMove)
     }
+
+    // cleanup
+    // cuando el componente se desmonta
+    // cuando cambia las dependecias, antes de ejecutar
+    // el efecto de nuevo
+    return () => {
+      console.log('cleanup')
+      window.removeEventListener('pointermove', handleMove)
+    }
   }, [enabled])
 
   return (
-    <main>
+    <>
       <div style={{
         position: 'absolute',
-        backgroundColor: '#09f',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
         borderRadius: '50%',
         opacity: 0.8,
         pointerEvents: 'none',
-        left: -20,
-        top: -20,
+        left: -25,
+        top: -25,
         width: 40,
         height: 40,
         transform: `translate(${position.x}px, ${position.y}px)`
@@ -34,6 +44,16 @@ function App() {
       <button onClick={() => setEnabled(!enabled)}>
         {enabled ? 'Desactivar' : 'Activar'} seguir puntero
       </button>
+    </>
+  )
+}
+
+function App() {
+  
+
+  return (
+    <main>
+      <FollowMouse />
     </main>
   )
 }
