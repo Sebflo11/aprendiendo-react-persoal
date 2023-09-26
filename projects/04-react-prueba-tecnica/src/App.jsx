922f9efe8819c9a143cfb3757a0c723fab7870a8
Sebastián Flores <sebflodev@gmail.com>
2023-09-25 23:11:54 -0300

    Seguimo con la clase

--- projects/04-react-prueba-tecnica/src/App.jsx
+++ projects/04-react-prueba-tecnica/src/App.jsx
@@ -1,21 +1,37 @@
 import { useEffect, useState } from "react"
+import './App.css'
 
 const CAT_ENDPOINT_RANDOM_FACT = 'https://catfact.ninja/fact'
 // const CAT_ENDPOINT_IMAGE_URL = `https://cataas.com/cat/says/${firstWord}?size=50&color=red&json=true`
+const CAT_PREFIX_IMAGE_URL = "https://cataas.com"
 
 export function App () {
     const [fact, setFact] = useState('lorem ipsum cat fact whatever')
+    const [imageUrl, setImageUrl] = useState()
 
     useEffect(() => {
         fetch(CAT_ENDPOINT_RANDOM_FACT)
             .then(res => res.json())
-            .then(data => setFact(data.fact))
+            .then(data => {
+                const { fact } = data
+                setFact(fact)
+
+                const threeFirstWord = fact.split(' ', 3).join(' ')
+
+                fetch(`https://cataas.com/cat/says/${threeFirstWord}?size=50&color=red&json=true`)
+                    .then(res => res.json())
+                    .then(response => {
+                        const { url } = response
+                        setImageUrl(url)
+                    })
+            })
     }, [])
 
     return (
         <main>
             <h1>App de gatitos</h1>
             {fact && <p>{fact}</p>}
+            {imageUrl && <img src={`${CAT_PREFIX_IMAGE_URL}${imageUrl}`} alt={`Image extracted using the first three words for ${fact}`}></img>}
         </main>
     )
 }
